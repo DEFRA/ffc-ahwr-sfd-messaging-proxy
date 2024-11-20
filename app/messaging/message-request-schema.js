9fef096de1205ab7007498ca3f8c55d188aff93d
Ben Hope <bendef24@gmail.com>
2024-11-20 10:02:20 +0000

    fix(AHWR-259): fixed logger.error message

--- app/messaging/message-request-schema.js
+++ app/messaging/message-request-schema.js
@@ -3,7 +3,7 @@ import { inboundMessageSchema } from '../schemas/index.js'
 export const validateMessageRequest = (logger, event) => {
   const { error } = inboundMessageSchema.validate(event, { abortEarly: false })
   if (error) {
-    logger.error(`Inbound message validation error, with message: ${error}`)
+    logger.error({ err: error }, 'Inbound message validation error')
     return false
   }
   return true
