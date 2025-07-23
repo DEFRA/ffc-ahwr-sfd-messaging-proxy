30a197632992360ff7470c9f62d0b94fd34c1341
Ben Hope <bendef24@gmail.com>
2025-07-23 10:43:20 +0100

    feat(AHWR-1067): Implement retention policy, added implementation

--- app/repositories/message-log-repository.js
+++ app/repositories/message-log-repository.js
@@ -1,4 +1,5 @@
 import dataModeller from '../data/index.js'
+import { Sequelize } from 'sequelize'
 
 export const set = async (data) => {
   const { models } = dataModeller
@@ -9,4 +10,61 @@ export const update = async (id, data) => {
   const { models } = dataModeller
   return models.messageLog.update(data,
     { where: { id } })
+}
+
+export const redactPII = async (agreementReference) => {
+  const { models } = dataModeller
+
+  // TODO 1067 move to shared lib
+  const REDACT_PII_VALUES = {
+    REDACTED_EMAIL: 'redacted.email@example.com'
+  }
+
+  const data = Sequelize.fn(
+          'jsonb_set',
+          Sequelize.fn(
+            'jsonb_set',
+            Sequelize.col('data'),
+            Sequelize.literal('\'{inboundMessage,emailAddress}\''),
+            Sequelize.literal(`'"${REDACT_PII_VALUES.REDACTED_EMAIL}"'`)
+          ),
+          Sequelize.literal('\'{outboundMessage,data,commsAddresses}\''),
+          Sequelize.literal(`'"${REDACT_PII_VALUES.REDACTED_EMAIL}"'`)
+      )
+
+  // eslint-disable-next-line no-unused-vars
+  // const [_, updates] = await models.application.update(
+  await models.messageLog.update(
+    { data },
+    {
+      where: { agreementReference },
+      returning: true
+    }
+  )
+
+  // TODO 1067 add later
+  // const [updatedRecord] = updates
+  // const { updatedAt, data: { organisation: { sbi } } } = updatedRecord.dataValues
+
+  // const eventData = {
+  //   applicationReference: reference,
+  //   reference,
+  //   updatedProperty,
+  //   newValue,
+  //   oldValue,
+  //   note
+  // }
+  // const type = `application-${updatedProperty}`
+  // await claimDataUpdateEvent(eventData, type, user, updatedAt, sbi)
+
+  // await buildData.models.claim_update_history.create({
+  //   applicationReference: reference,
+  //   reference,
+  //   note,
+  //   updatedProperty,
+  //   newValue,
+  //   oldValue,
+  //   eventType: type,
+  //   createdBy: user
+  // })
 }
--- app/routes/redact-pii.js
+++ app/routes/redact-pii.js
@@ -1,11 +1,17 @@
 import HttpStatus from 'http-status-codes'
+import { redactPII } from '../repositories/message-log-repository.js'
 
 export const redactPiiRequestHandlers = [
   {
     method: 'POST',
     path: '/api/redact/pii',
     handler: async (request, h) => {
-      request.logger.info(`Request for redact PII received, agreementsToRedact: ${JSON.stringify(request.payload)}`)
+      request.logger.info('Request for redact PII received')
+
+      request.payload.agreementsToRedact.forEach(agreementToRedact => {
+        redactPII(agreementToRedact.reference)
+      })
+
       return h.response().code(HttpStatus.OK)
     }
   }
