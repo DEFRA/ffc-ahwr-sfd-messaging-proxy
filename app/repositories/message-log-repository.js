cfd67383cd3047a1d720fafe74f9c7c3174712fa
Ben Hope <bendef24@gmail.com>
2025-08-07 13:56:25 +0100

    feat(AHWR-1067): Implement retention policy, added implementation (#29)

--- app/repositories/message-log-repository.js
+++ app/repositories/message-log-repository.js
@@ -1,4 +1,6 @@
+import { REDACT_PII_VALUES } from 'ffc-ahwr-common-library'
 import dataModeller from '../data/index.js'
+import { Sequelize, Op } from 'sequelize'
 
 export const set = async (data) => {
   const { models } = dataModeller
@@ -9,4 +11,45 @@ export const update = async (id, data) => {
   const { models } = dataModeller
   return models.messageLog.update(data,
     { where: { id } })
+}
+
+export const redactPII = async (agreementReference, logger) => {
+  const { models } = dataModeller
+
+  const redactedValueByJSONPath = {
+    'inboundMessage,emailAddress': REDACT_PII_VALUES.REDACTED_EMAIL,
+    'outboundMessage,data,commsAddresses': REDACT_PII_VALUES.REDACTED_EMAIL
+  }
+
+  let totalUpdates = 0
+
+  for (const [jsonPath, redactedValue] of Object.entries(redactedValueByJSONPath)) {
+    const jsonPathSql = jsonPath.split(',').map(key => `->'${key}'`).join('')
+
+    const [affectedCount] = await models.messageLog.update(
+      {
+        data: Sequelize.fn(
+          'jsonb_set',
+          Sequelize.col('data'),
+          Sequelize.literal(`'{${jsonPath}}'`),
+          Sequelize.literal(`'"${redactedValue}"'`)
+        )
+      },
+      {
+        where: {
+          agreementReference,
+          [Op.and]: Sequelize.literal(`data${jsonPathSql} IS NOT NULL`)
+        }
+      }
+    )
+
+    totalUpdates += affectedCount
+    logger.info(`Redacted field at path '${jsonPath}' in ${affectedCount} message(s) for agreementReference: ${agreementReference}`)
+  }
+
+  if (totalUpdates > 0) {
+    logger.info(`Total redacted fields across messages: ${totalUpdates} for agreementReference: ${agreementReference}`)
+  } else {
+    logger.info(`No messages updated for agreementReference: ${agreementReference}`)
+  }
 }
--- app/routes/redact-pii.js
+++ app/routes/redact-pii.js
@@ -1,11 +1,18 @@
 import HttpStatus from 'http-status-codes'
+import { redactPII } from '../repositories/message-log-repository.js'
 
 export const redactPiiRequestHandlers = [
   {
     method: 'POST',
     path: '/api/redact/pii',
     handler: async (request, h) => {
-      request.logger.info(`Request for redact PII received, agreementsToRedact: ${JSON.stringify(request.payload)}`)
+      request.logger.info('Request for redact PII received')
+
+      await Promise.all(
+        request.payload.agreementsToRedact.map(async (agreementToRedact) => {
+          await redactPII(agreementToRedact.reference, request.logger)
+        }))
+
       return h.response().code(HttpStatus.OK)
     }
   }
